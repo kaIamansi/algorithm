7cd8586f24d19c466671908afb1c46b3181eeb68
hagyeong <gkrud4513@naver.com>
2020-05-25 14:08:15 +0900

    권하경 알고리즘 수정

--- 알고리즘/권하경/pyNumber.js
+++ 알고리즘/권하경/pyNumber.js
@@ -1,9 +1,6 @@
 function solution(s){
-  var answer = true;
   s = s.toLowerCase().split('');  
-
-  if(s.filter(word=> word=='p').length!=s.filter(word=> word=='y').length) answer = false;
    
-  return answer;
+  return s.filter(word=> word=='p').length==s.filter(word=> word=='y').length;
 }
 
