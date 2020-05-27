bcba81bcd26eb45aa1cfdc0b9066144321ed1c14
alton15 <alton15@naver.com>
2020-05-27 14:21:04 +0900

    5월 27일 알고리즘 / 타일 장식물

---
+++ 알고리즘/김기강/20.05.27 TileOrnaments/TileOrnaments.java
@@ -0,0 +1,12 @@
+public long solution(int N) {
+        long[] arr = new long[N];
+
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for(int i = 2; i < N; i++) {
+	arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        return (4 * arr[N-1]) + (2 * arr[N-2]);
+}
