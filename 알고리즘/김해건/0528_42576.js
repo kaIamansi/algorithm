aca1e0c4474c571598205e7260f4bf397b7fc12f
gorjs3540 <gorjs3540@gmail.com>
2020-05-28 15:44:34 +0900

    [수정] 코드 수정

--- 알고리즘/김해건/0528_42576.js
+++ 알고리즘/김해건/0528_42576.js
@@ -2,9 +2,7 @@ function solution(participant, completion) {
     let answer = participant.sort();
     let remove = completion.sort();
     
-    for (let i=0; i<answer.length; i++) {
-        if (answer[i] != remove[i]) {
+    for (let i=0; i<answer.length; i++)
+        if (answer[i] != remove[i]) 
             return answer[i];
-        }
-    }
 }
