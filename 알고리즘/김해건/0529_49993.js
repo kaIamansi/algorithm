bc12e1f057de213211664ec2f8b7ab9bfde74268
gorjs3540 <gorjs3540@gmail.com>
2020-05-29 15:44:07 +0900

    [추가] 스킬트리

---
+++ 알고리즘/김해건/0529_49993.js
@@ -0,0 +1,18 @@
+function solution(skill, skill_trees) {
+    let compare = '', result = '';
+    let count = 0;
+    
+    for (let i=0; i<skill_trees.length; i++) {
+        compare = skill_trees[i];
+        result = '';
+        
+        for (let j=0; j<compare.length; j++) 
+            if (skill.search(compare[j]) != -1)
+                result += compare[j];
+        
+        if (result == skill || skill.search(result) == 0) 
+            count++;
+    }
+    
+    return count;
+}
