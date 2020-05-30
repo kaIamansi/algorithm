e2a09314e607d1f6e75df1c78a9724b44cffa117
nwar-Jeon <nwar0825@naver.com>
2020-05-30 23:49:30 +0900

    2020/06/01 코드 수정

--- 알고리즘/전승민/bestAlbum/src/BestAlbum.kt
+++ 알고리즘/전승민/bestAlbum/src/BestAlbum.kt
@@ -1,50 +1,38 @@
 import java.util.*
 
-fun main() {
-    val genres = arrayOf("classic", "pop", "classic", "classic", "pop")
-    val plays = intArrayOf(1500, 200, 150, 800, 2200)
-    solution(genres, plays).forEach {
-        println(it)
-    }
-}
+fun solution(genres: Array<String>, plays: IntArray): IntArray
+    = getResult(getInputData(genres, plays))
 
-fun solution(genres: Array<String>, plays: IntArray): IntArray {
+fun getInputData(genres: Array<String>, plays: IntArray) : HashMap<String, Pair<Int, TreeSet<Pair<Int, Int>>>> {
     val genreMap = HashMap<String, Pair<Int, TreeSet<Pair<Int, Int>>>>()
     val sorting: (Pair<Int, Int>, Pair<Int, Int>) -> Int = { a, b ->
         if (a.first.compareTo(b.first) == 0) b.second.compareTo(a.second) else a.first.compareTo(b.first)
     }
 
     for (i in plays.indices) {
-        if (genreMap.containsKey(genres[i])) { // 장르정보가 이미 있는경우에 실행
-            val pair = genreMap[genres[i]]!!
-            val set = pair.second
-            set.add(plays[i] to i)
-            genreMap[genres[i]] = pair.copy(first = pair.first + plays[i], second = set)
-            // 장르 총 재생횟수 증가, 해당 노래의 인덱스와 재생횟수 저장
-        } else { // 해당 장르 데이터가 없을 경우 실행
-            val set = TreeSet<Pair<Int, Int>>(sorting)
-            set.add(plays[i] to i)
-            genreMap[genres[i]] = plays[i] to set
-            // 장르 정보 생성, 해당 노래의 인덱스와 재생횟수 저장
+        genreMap[genres[i]] = if (genreMap.containsKey(genres[i])) { // 장르정보가 이미 있는경우 장르 총 재생 수 저장
+            genreMap[genres[i]]!!.first + plays[i] to genreMap[genres[i]]!!.second
+        } else { // 해당 장르 데이터가 없을 경우 장르 총 재생 수 저장, TreeSet 초기화
+            plays[i] to TreeSet(sorting)
+        }.apply {// 노래의 재생 수, 인덱스 저장
+            this.second.add(plays[i] to i)
         }
     }
 
+    return genreMap
+}
+
+fun getResult(genreMap : HashMap<String, Pair<Int, TreeSet<Pair<Int, Int>>>>) : IntArray {
     val array = arrayListOf<Int>()
-    genreMap.values.sortedBy {
+    genreMap.values.sortedBy {// 장르별 재생횟수 총합을 기준으로 정렬
         it.first
     }.asReversed().forEach {
-        /*if(it.second.size==1) array.add(it.second.pollLast()!!.second)
-        while(it.second.size>1) {
-            array.add(it.second.pollLast()!!.second)
-            array.add(it.second.pollLast()!!.second)
-        }*/
         it.second.pollLast()?.apply {
             array.add(this.second)
         }
         it.second.pollLast()?.apply {
             array.add(this.second)
-        }
+        } // 값 존재 여부 확인 후 배열에 추가.
     }
-
     return array.toIntArray()
 }
