d67c3dbc0f1da56fc62e880276ad12f739da54b0
gorjs3540 <gorjs3540@gmail.com>
2020-06-02 19:41:15 +0900

    [수정] 코드 수정

--- 알고리즘/김해건/0602_64061.js
+++ 알고리즘/김해건/0602_64061.js
@@ -1,13 +1,10 @@
 function solution(board, moves) {
-    let count = 0;
     let order = new Array();
     let newBoard = new Array();
-    let getdoll = new Array();
-    let result = new Array();
-    const len = board.length;
+    let dolls = new Array();
     
-    for(let i=0; i<len; i++) {
-        for(let j=0; j<len; j++)
+    for(let i=0; i<board.length; i++) {
+        for(let j=0; j<board.length; j++)
             order.push(board[j][i]);
         newBoard.push(order);
         order = [];
@@ -15,16 +12,16 @@ function solution(board, moves) {
     // 열을 기준으로 인형을 세는 배열 생성
 
     for (let i=0; i<moves.length; i++) {
-        getdoll.push(newBoard[moves[i]-1].find(item => { return item !=0 }));
+        dolls.push(newBoard[moves[i]-1].find(item => { return item !=0 }));
         newBoard[moves[i]-1].splice(newBoard[moves[i]-1]
         .findIndex(item => item != 0 ), 1, 0);
     }
     
-    let filtered = getdoll.filter(item => item != undefined);
+    let filtered = dolls.filter(item => item != undefined);
     let answer = filtered.length;
     
     for (let i=0; i<filtered.length; i++) {
-        filtered.forEach((item,index, arr) => { 
+        filtered.forEach((item,index,arr) => { 
         if (item == arr[index+1])
             arr.splice(index, 2);
         });
