1189daede36405b657fd264480532f112ae3886f
hagyeong <gkrud4513@naver.com>
2020-06-03 14:44:14 +0900

    권하경 알고리즘

---
+++ 알고리즘/권하경/20-06-03/20-06-03.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  var answer = [];
+  answer = sort(answer,n);
+  return answer;
+}
+
+function sort(answer,n) {
+  if(n==1){
+    answer.push(0);
+    return answer;
+  }else{
+    answer = sort(answer,--n);
+    let b = answer.map(x=>parseInt((x-1)*-1));
+    b.reverse();
+    
+    answer.push(0);
+    return answer.concat(b);
+  }
+}
