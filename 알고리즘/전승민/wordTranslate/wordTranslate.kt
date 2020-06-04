9407ff42875192ce335758a341247c35cb9191fe
nwar-Jeon <nwar0825@naver.com>
2020-06-04 10:56:29 +0900

    2020/06/04 단어 변환 코드 수정

--- 알고리즘/전승민/wordTranslate/wordTranslate.kt
+++ 알고리즘/전승민/wordTranslate/wordTranslate.kt
@@ -1,43 +1,33 @@
-fun main() {
-    val string = "hit"
-    val arr = arrayOf("hot", "dot", "dog", "lot", "log", "cog")
-    val target = "cog"
-    println(solution(string, target, arr))
-}
+import kotlin.math.min
 
 val infinity = 987654321
 
-fun solution(begin: String, target: String, words: Array<String>): Int {
-    val length = words.size
-    val result = dps(begin, target, Array(length) { false }, words)
-    return if(result==infinity) 0 else result
-}
+fun solution(begin: String, target: String, words: Array<String>): Int
+    = dps(begin, target, Array(words.size) { false }, words).let {
+        if(it==infinity) 0 else it
+    }
 
 fun dps(cur: String, target: String, visit: Array<Boolean>, words: Array<String>): Int {
     if (cur == target) {
         return 0
     }
-    var min = infinity
+    var minimum = infinity
     words.indices.forEach {
         if (!visit[it]) {
             visit[it] = true
-            if (cur.isOneLetterDiff(words[it])) {
-                val value = dps(words[it], target, visit, words) + 1
-                if (min > value) min = value
-            }
+            minimum = if (cur.isOneLetterDiff(words[it])) min(dps(words[it], target, visit, words) + 1, minimum) else minimum
             visit[it] = false
         }
     }
-    return min
+    return minimum
 }
 
 fun String.isOneLetterDiff(other: String): Boolean {
-    val length = this.length
     var count = 0
     this.indices.forEach {
         if (this[it] == other[it]) count++
     }
-    return length - 1 == count
+    return this.length - 1 == count
 }
 
 val String.indices
