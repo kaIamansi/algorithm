31a7bfa91173ecdffbcd25cdc206f9f1eca24cbc
unknown <leaf_cis@naver.com>
2020-06-08 17:14:15 +0900

    두 줄 줄이기

--- 알고리즘/최이삭/Printer.js
+++ 알고리즘/최이삭/Printer.js
@@ -1,15 +1,13 @@
 function Print(priorities, location) {
   let copy = [...priorities].slice(1);
-  let print = 0;
   if(priorities.find(el => el > priorities[0]) === undefined) {
-      if(location === 0) return 1;
-      else print = Print(copy, location - 1) + 1;
+      if(location === 0) return 1; // 같으면 1 리턴
+      else return Print(copy, location - 1) + 1; // Print 재귀 호출 후 나온 리턴 값에 1 더함.
   }
   else {
-      if(location === 0) print = Print(copy.concat([...priorities].shift()), copy.length)
-      else print = Print(copy.concat([...priorities].shift()), location - 1)
+      if(location === 0) return Print(copy.concat([...priorities].shift()), copy.length)
+      else return Print(copy.concat([...priorities].shift()), location - 1)
   }
-  return print;
 }
 
 function solution(priorities, location) {
