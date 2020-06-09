69d0413e32a5de48c11b650ed3a11b7b1831944b
nwar-Jeon <nwar0825@naver.com>
2020-06-09 09:03:21 +0900

    2020/06/09 체육복

---
+++ 알고리즘/전승민/workoutClothes/workoutClothes.kt
@@ -0,0 +1,28 @@
+fun main() {
+    val n = 3
+    val lost = intArrayOf(3)
+    val reserve = intArrayOf(1)
+    println(solution(n,lost,reserve))
+}
+
+fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+    val clothesArr = IntArray(n) {1}
+    lost.forEach {
+        clothesArr[it-1]--
+    }
+    reserve.forEach {
+        clothesArr[it-1]++
+    }
+    clothesArr.forEachIndexed { index, i ->
+        if(i==0) {
+            if(index!=0 && clothesArr[index-1]==2) {
+                clothesArr[index-1]--
+                clothesArr[index]++
+            } else if(index!=clothesArr.size-1 && clothesArr[index+1]==2) {
+                clothesArr[index+1]--
+                clothesArr[index]++
+            }
+        }
+    }
+    return clothesArr.filter { it > 0 }.size
+} // https://programmers.co.kr/learn/courses/30/lessons/42862
