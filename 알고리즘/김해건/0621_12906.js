0cd01dfbdf5997380b8f1f5232b97507ff18bc6d
Sun_Gun <65691192+gorjs3540@users.noreply.github.com>
2020-06-23 11:37:47 +0900

    Delete 0621_12906.js

--- 알고리즘/김해건/0621_12906.js
+++
@@ -1,9 +0,0 @@
-function solution(arr) {
-    let answer = new Array();
-
-    answer = arr.filter((item, index, array) => {
-        return item != array[index+1];
-    });
-
-    return answer;
-}
