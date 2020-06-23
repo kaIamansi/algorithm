7d9b41dc7ddb3f0a4b19aca10a5d092cbbf19d2b
nwar-Jeon <nwar0825@naver.com>
2020-06-23 12:01:24 +0900

    2020/06/23 코드수정

--- 알고리즘/전승민/README.md
+++ 알고리즘/전승민/README.md
@@ -6,18 +6,12 @@
 ```kotlin
 fun solution(stones: IntArray, k: Int): Int {
     var answer = 987654321
-    var index = -1
-    while (index < stones.size) {
-        var max = index + 1
-        for (i in index + 2..index + k) {
-            if (i >= stones.size) break
-            if (stones[max] < stones[i]) max = i
+        var index = -1
+        while (index < stones.size - k) {
+            index = (index+1..index+k).maxBy { stones[it] } ?: -1
+            if(answer > stones[index]) answer = stones[index]
         }
-        if (stones.size - k <= index) break
-        index = max
-        if (answer > stones[index]) answer = stones[index]
-    }
-    return answer
+        return answer
 }
 ```
 + 풀이
--- 알고리즘/전승민/steppingStoneCrossing/SteppingStoneCrossing.kt
+++ 알고리즘/전승민/steppingStoneCrossing/SteppingStoneCrossing.kt
@@ -1,17 +1,26 @@
 fun steppingStoneCrossing(stones: IntArray, k: Int): Int {
     var answer = 987654321
     var index = -1
-    while (index < stones.size) {
-        var max = index + 1
-        for (i in index + 2..index + k) {
-            if (i >= stones.size) break
-            if (stones[max] < stones[i]) max = i
-        }
-        if (stones.size - k <= index) break
-        index = max
-        if (answer > stones[index]) answer = stones[index]
+    while (index < stones.size - k) {
+        index = (index+1..index+k).maxBy { stones[it] } ?: -1
+        if(answer > stones[index]) answer = stones[index]
     }
     return answer
 }
-
 // https://programmers.co.kr/learn/courses/30/lessons/64062
+
+/*
+fun solution(stones: IntArray, k: Int): Int {
+    var answer = infinity
+    var index = -1
+    while (index < stones.size-k) {
+        var max = index + 1
+        for (i in index + 2..index + k) {
+            if (stones[max] <= stones[i]) max = i
+        }
+        index = max
+        if(answer > stones[index]) answer = stones[index]
+    }
+    return answer
+}
+ */
