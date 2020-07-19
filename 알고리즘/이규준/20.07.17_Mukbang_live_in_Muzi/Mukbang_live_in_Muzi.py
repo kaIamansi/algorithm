03a6f43d65c8853485f96e2e42d28da1dae8c750
iopasd753951 <iopasd753951@gmail.com>
2020-07-19 23:45:55 +0900

    [추가] 20.07.17문제 해결중...

--- 알고리즘/이규준/20.07.17_Mukbang_live_in_Muzi/Mukbang_live_in_Muzi.py
+++ 알고리즘/이규준/20.07.17_Mukbang_live_in_Muzi/Mukbang_live_in_Muzi.py
@@ -1,22 +1,38 @@
 def solution(food_times, k):
     buffer = []
 
-    if sum(food_times) < k:
+    if sum(food_times) <= k:
         return -1
 
     for i in range(len(food_times)):
-        buffer.append([food_times[i], i])
-
-    buffer.sort()
+        buffer.append([food_times[i], i + 1])
 
     while True:
-        if k > k - len(food_times) * len(buffer):
+        min_time = min(buffer)[0]
+        print(len(buffer))
 
+        buffer.sort(reverse=True)
 
+        if k > k - len(buffer) * min(buffer)[0]:
+            k = k - len(buffer) * min(buffer)[0]
+            print("d여기")
+            for i in range(len(buffer)):
+                if min_time == buffer[-1][0]:
+                    del buffer[-1]
+                    print("asdf", buffer)
+                else:
+                    print("브레이크")
+                    break
 
+        elif k < k - len(buffer) * min(buffer)[0]:
+            buffer = sorted(buffer, key=lambda x: x[1])
+            print(buffer)
+            for i in range(len(buffer)):
+                k -= 1
+                if k == 0:
+                    buffer.append(buffer[0])
+                    return buffer[i + 1][1]
 
-
-
-
+        print("시발")
 
     return 0
